15a51447c8427dc533d2de2e159443118ea655d4
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2020-10-22 00:06:21 -0700

    Got a basic Alexa response working. Proof of concept on Azure.

--- RefugeeRestrooms_AlexaSkillsKit_Trigger/index.js
+++ RefugeeRestrooms_AlexaSkillsKit_Trigger/index.js
@@ -1,14 +1,42 @@
+require("app-module-path").addPath(__dirname);
+
+const Alexa = require("ask-sdk-core");
+
+const HelloIntentHandler = {
+    canHandle(handlerInput) {
+        return true;
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak("Hi there Alexa")
+            .withShouldEndSession(true)
+            .getResponse();
+    }
+};
+
+const LogResponseInterceptor = {
+    process(handlerInput, response) {
+        console.log(`RESPONSE = ${JSON.stringify(response)}`);
+    },
+};
+
+let skill;
 module.exports = async function (context, event) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    if (!skill) {
+        skill = Alexa.SkillBuilders.custom()
+            .addRequestHandlers(
+                HelloIntentHandler)
+            .addResponseInterceptors(
+                LogResponseInterceptor,
+            )
+            .withApiClient(new Alexa.DefaultApiClient())
+            .create();
+    }
 
-    const name = (event.query.name || (event.body && event.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". Modified Code. This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    const alexaResponse = `{"version":"1.0","response":{"outputSpeech":{"type":"SSML","ssml":"<speak>I found this positively rated restroom 0.36 miles away. <s>Pagliacci Pizza<\/s> <say-as interpret-as=\"address\"> 3015 E Madison <\/say-as>, Seattle. I also sent this and more restrooms to your email.<\/speak>"}},"shouldEndSession":true}`;
+    const response = await skill.invoke(event.body, context);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: alexaResponse
+        status: 200,
+        body: response
     };
+    return;
 }
